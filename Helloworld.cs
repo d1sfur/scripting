3d3733cd511a6abcc1c865684d5ee37d5d7e5aa4
d1sfur <evapolakova435@gmail.com>
2026-05-26 20:26:49 +0700

    Add files via upload

---
+++ Helloworld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helloworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Вывод текста в начале игры
+        print("Papsdpa");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
